bc66f27bfacd85ce62980445f8977c1167dc9f9e
Charlie Mayer <charliemayer3@gmail.com>
2018-01-26 09:46:24 -0700

    cleaned up the syntax a bit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,40 @@
 $( document ).ready(function(){
 
-	var totalScore = 0; 
+	var totalScore; 
 	var wins = 0;
 	var losses = 0;
-	var randomTotal = Math.floor(Math.random() * 102) + 19;
-	var crystalArray = []
-	for(var i = 0; i < 4; i++) {
-		crystalArray.push(Math.floor(Math.random() * 12) + 1);
+	var randomTotal;
+	var crystalArray; 
+
+	// runs reset function to start the game which sets all of the variables.
+	reset()
+
+	// reset function.  reandomly selects new crystal values and randomTotal goal, and resets the totalScore to 0. 
+	function reset () {
+		totalScore = 0;
+		$(".totalScore").html(totalScore);
+		crystalArray = []
+		for(var i = 0; i < 4; i++) {
+			crystalArray.push(Math.floor(Math.random() * 12) + 1);
+		}
+		randomTotal = Math.floor(Math.random() * 102) + 19;
+		$("#targetScore").html(randomTotal);
 	}
 
 	// jquery html change commands.
 	$("#targetScore").html(randomTotal);
-	
-	// console.log(randomTotal);
-	// console.log(crystalArray);
 
 	// click function
 	$(".crystalImage").on("click", function() {
-		var index = $(this).attr("value");   // sets the variable 'index' to connect with the values inside the html images 
-		// console.log(index);
+		var index = $(this).attr("value");   // sets the variable 'index' to connect with the values inside the html images.
 		totalScore = totalScore + crystalArray[index - 1];  // take the previous totalScore and adds the value now associated with the button clicked.  "index - 1" to associate with the first member of the crystalArray.
-		// console.log(totalScore);
 	    $(".totalScore").html(totalScore); // updates the totalScore on the page
-	        if(totalScore == randomTotal){  // sets the winning condition which would call the winner function.
-	        	winner();
-	        }
-	        else if(totalScore > randomTotal){  // sets the losing condition with calls the loser function.
-	        	loser();
-	        }   
+	    if(totalScore == randomTotal){  // sets the winning condition which would call the winner function.
+	        winner();
+	    }
+	    else if(totalScore > randomTotal){  // sets the losing condition with calls the loser function.
+	        loser();
+	    }   
 	})
 
 	// sets my winner function.  adds a win, alerts a win, and runs reset function.
@@ -46,18 +53,4 @@ $( document ).ready(function(){
 		reset();
 	}
 
-	// reset function.  reandomly selects new crystal values and randomTotal goal, and resets the totalScore to 0. 
-	function reset () {
-		totalScore = 0;
-		$(".totalScore").html(totalScore);
-		crystalArray = []
-			for(var i = 0; i < 4; i++) {
-				crystalArray.push(Math.floor(Math.random() * 12) + 1);
-			}
-		randomTotal = Math.floor(Math.random() * 102) + 19;
-		$("#targetScore").html(randomTotal);
-		
-		// console.log(crystalArray);
-		// console.log(randomTotal);
-	}
 });
